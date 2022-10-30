2196353613a9780ee8f79cc926711df57a91dab8
김병주 <atker14@gmail.com>
2022-10-30 18:50:04 +0900

    cobine_csv : multiprocessing

--- combine_csv/README.md
+++ combine_csv/README.md
@@ -1,6 +1,6 @@
 # combine_csv
 ```bash
-python --paths csv_paths --output_path output_csv_path --encode loaded_csv_encode --output_encode saved_csv_encode
+python --paths csv_paths --output_path output_csv_path --encode loaded_csv_encode --output_encode saved_csv_encode --process_count process_for_multiprocessing
 ```
   
 ## flag  
@@ -8,8 +8,10 @@ python --paths csv_paths --output_path output_csv_path --encode loaded_csv_encod
 --output_path, -op 
 --encode, -e
 --output_encode, -oe  
+--process_count, -pc
   
   
 ## feature  
 파일별로 하나의 컬럼을 가지는 csv를 모아 하나의 파일로 변환한다
+병렬처리를 통하여 속도를 높임
     
--- combine_csv/combine_csv.py
+++ combine_csv/combine_csv.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import glob
-from tqdm import tqdm
 import argparse
+from tqdm import tqdm
+from multiprocessing import Pool
 
 parser = argparse.ArgumentParser(description='get csv')
 
@@ -9,9 +10,20 @@ parser.add_argument("--paths", "-p", help= "target csv paths \n ex) *.csv")
 parser.add_argument("--output_path", "-op", help= "output csv path \n ex) output.csv")
 parser.add_argument("--encode", "-e", default= "cp949", help= "target csv encoding")
 parser.add_argument("--output_encode", "-oe", default="utf-8", help= "output encoding")
+parser.add_argument("--process_count", "-pc", default= 4, type= int, help= "process for multiprocessing")
 
 args = parser.parse_args()
 
-all_filenames = [i for i in glob.glob(args.paths)]
-combine_csv = pd.concat([pd.read_csv(x, encoding= args.encode) for x in tqdm(all_filenames)])
-combine_csv.to_csv(args.output_path, index= False, encoding= args.output_encode)
+def combine_csv(all_filename) :
+    result = pd.read_csv(all_filename, encoding= args.encode)
+    return result
+
+if __name__ == '__main__':
+    all_filenames = [x for x in glob.glob(args.paths)]
+    pool = Pool(processes= args.process_count)
+    combine_csv = pd.concat(pool.map(combine_csv, tqdm(all_filenames)))
+    pool.close()
+    pool.join()
+    
+    combine_csv.to_csv(args.output_path, index= False, encoding= args.output_encode)
+    print("Complete!")
